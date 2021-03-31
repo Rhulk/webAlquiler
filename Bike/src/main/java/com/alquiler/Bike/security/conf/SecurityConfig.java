b9eb53e65804826ab1c81d29da2912b370b20fe9
kk <quique1904@gmail.com>
2021-03-31 18:08:02 +0200

    Security Basic

---
+++ Bike/src/main/java/com/alquiler/Bike/security/conf/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.alquiler.Bike.security.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;	
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration	
+@EnableWebSecurity	
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	// falta la pagina para redireccionar si no hay permisos
+	@Override		
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("user").password("{noop}1234").roles("USER")
+		.and()
+		.withUser("admin").password("{noop}1234").roles("ADMIN");
+	}	
+	
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        	.antMatchers("/contac").hasRole("ADMIN")
+            .antMatchers("/").hasRole("USER")
+            .and().formLogin()
+            .and().logout();
+    }
+	
+}   
